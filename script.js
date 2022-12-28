30ae5c596896f5c28d3921b0bc6bb5a4fc7d06e8
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 23:58:48 +0200

    finish

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// //min
-// //0.1 + 0.2
-// let a = 0.1
-// let b = 0.2
-// let c = (a + b).toFixed(1)
-// alert(`${a} + ${b} = ${c}`)
+//min
+//0.1 + 0.2
+let a = 0.1
+let b = 0.2
+let c = (a + b).toFixed(1)
+alert(`${a} + ${b} = ${c}`)
 
-// //"1" + 2
-// let num1 = "1"
-// let num2 = 2
-// let num3 = Number(num1) + num2
-// alert(`${num3}`)
+//"1" + 2
+let num1 = "1"
+let num2 = 2
+let num3 = Number(num1) + num2
+alert(`${num3}`)
 
-// //flash-drive
-// let flashSize = prompt('Size of the flash-drive is (in gb)')
-// let amountOfFiles = Math.floor((flashSize * 1000) / 820)
-// alert(`${amountOfFiles} can be downloaded on the flash-drive`)
+//flash-drive
+let flashSize = prompt('Size of the flash-drive is (in gb)')
+let amountOfFiles = Math.floor((flashSize * 1000) / 820)
+alert(`${amountOfFiles} can be downloaded on the flash-drive`)
 
-// //normal
-// //chocolate
-// let amountOfMoney = prompt('How much money do you have?', '')
-// let chocolatePrice = prompt('Price of the chocolate you want', '')
-// let amountOfChocolate = Math.floor(amountOfMoney / chocolatePrice)
-// let change = amountOfMoney % chocolatePrice
-// alert(`You can buy ${amountOfChocolate} chocolate bars`)
-// alert(`This is your change: ${change}`)
+//normal
+//chocolate
+let amountOfMoney = prompt('How much money do you have?', '')
+let chocolatePrice = prompt('Price of the chocolate you want', '')
+let amountOfChocolate = Math.floor(amountOfMoney / chocolatePrice)
+let change = amountOfMoney % chocolatePrice
+alert(`You can buy ${amountOfChocolate} chocolate bars`)
+alert(`This is your change: ${change}`)
 
-// //triple
-// let tripleNum = prompt('Type in your tree-digit number', '')
-// let reverseNum1 = tripleNum % 10
-// num2 = parseInt(tripleNum / 10)
-// let reverseNum2 = num2 % 10
-// num3 = parseInt(num2 / 10)
-// let reverseNum3 = num3 % 10
-// alert(`Reverse number is ${reverseNum1}${reverseNum2}${reverseNum3}`)
+//triple
+let tripleNum = prompt('Type in your tree-digit number', '')
+let reverseNum1 = tripleNum % 10
+num2 = parseInt(tripleNum / 10)
+let reverseNum2 = num2 % 10
+num3 = parseInt(num2 / 10)
+let reverseNum3 = num3 % 10
+alert(`Reverse number is ${reverseNum1}${reverseNum2}${reverseNum3}`)
 
 //max
 //bank
