2c85527f2637565f2c70c660eecde8ad3d2eee5d
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 23:15:58 +0200

    change //min

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-//min
-//0.1 + 0.2
-let a = 0.1
-let b = 0.2
-let c = a + b
-alert(`${a} + ${b} = ${c}`)
+// //min
+// //0.1 + 0.2
+// let a = 0.1
+// let b = 0.2
+// let c = (a + b).toFixed(1)
+// alert(`${a} + ${b} = ${c}`)
+
+//"1" + 2
+let num1 = "1"
+let num2 = 2
+let num3 = Number(num1) + num2
+alert(`${num3}`)
