36a1a52778612fe9ad40274ba996a7d439b84568
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 23:23:32 +0200

    change //min

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 // let c = (a + b).toFixed(1)
 // alert(`${a} + ${b} = ${c}`)
 
-//"1" + 2
-let num1 = "1"
-let num2 = 2
-let num3 = Number(num1) + num2
-alert(`${num3}`)
+// //"1" + 2
+// let num1 = "1"
+// let num2 = 2
+// let num3 = Number(num1) + num2
+// alert(`${num3}`)
+
+//flash-drive
+let flashSize = prompt('Size of the flash-drive is (in gb)')
+let amountOfFiles = Math.floor((flashSize * 1000) / 820)
+alert(`${amountOfFiles} can be downloaded on the flash-drive`)
