b94e5ece1b067643ba3fb6f95061cfcf88272465
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 23:50:23 +0200

    change //normal

--- script.js
+++ script.js
@@ -16,11 +16,20 @@
 // let amountOfFiles = Math.floor((flashSize * 1000) / 820)
 // alert(`${amountOfFiles} can be downloaded on the flash-drive`)
 
-//normal
-//chocolate
-let amountOfMoney = prompt('How much money do you have?', '')
-let chocolatePrice = prompt('Price of the chocolate you want', '')
-let amountOfChocolate = Math.floor(amountOfMoney / chocolatePrice)
-let change = amountOfMoney % chocolatePrice
-alert(`You can buy ${amountOfChocolate} chocolate bars`)
-alert(`This is your change: ${change}`)
+// //normal
+// //chocolate
+// let amountOfMoney = prompt('How much money do you have?', '')
+// let chocolatePrice = prompt('Price of the chocolate you want', '')
+// let amountOfChocolate = Math.floor(amountOfMoney / chocolatePrice)
+// let change = amountOfMoney % chocolatePrice
+// alert(`You can buy ${amountOfChocolate} chocolate bars`)
+// alert(`This is your change: ${change}`)
+
+//triple
+let tripleNum = prompt('Type in your tree-digit number', '')
+let reverseNum1 = tripleNum % 10
+num2 = parseInt(tripleNum / 10)
+let reverseNum2 = num2 % 10
+num3 = parseInt(num2 / 10)
+let reverseNum3 = num3 % 10
+alert(`Reverse number is ${reverseNum1}${reverseNum2}${reverseNum3}`)
